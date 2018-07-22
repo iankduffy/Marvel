20389ab9a37a7efd587ea77e26e2ae439c5964fe
Ian <i.duffy7999@gmail.com>
2018-07-22 21:05:26 +0100

    Added Heros page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import '../styles/Main.scss';
-
+import '../styles/Hero-page.scss';
+import Herosdata from '.././data/super-heros.json';
 
 // https://stackoverflow.com/questions/47837221/react-router-4-path-with-parameter
 
@@ -8,12 +8,21 @@ class Heros extends Component {
   constructor(props){
     super(props)
   }
+
   render() {
-    console.log(this.props)
+    const heroURL = this.props.match.url;
+    const Heros = Herosdata.heros;
+    
+    const data = Heros.find(hero =>{
+      return hero.url === heroURL
+    })
+    const Background = require(`./../images/heros/${data.smallImg}`); 
     return (
-      <div>
-        <h1>Hero</h1>
-        <p>{this.props.match.params.id}</p>
+      <div className="hero-page" style={{backgroundImage: `url(${Background})`}}>
+        <div className="intro">
+        <h1>{data.name}</h1>
+        <p>{data.description}</p>
+        </div>
       </div>
     );
   }
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import '../styles/Main.scss';
 import { Link } from 'react-router-dom';
 import HeroLink from './Link'
-import Heros from '.././data/super-heros.json'
+import Heros from '.././data/super-heros.json'; 
+import Background from '.././images/heros/spiderman.jpg'; 
 
 // https://stackoverflow.com/questions/47837221/react-router-4-path-with-parameter
 
@@ -20,12 +21,11 @@ class Homepage extends Component {
     // console.log(data[0].name); 
 
     return (
-      <div>
+      <div style={{backgroundImage: `url(${Background})`}}>
         <div className="intro">
           <h1>MARVEL AVENGERS</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam efficitur orci in risus blandit pellentesque. Nunc aliquam augue vitae mi molestie maximus. Pellentesque ut vestibulum nibh. Ut volutpat consequat magna sed vestibulum. Duis nec erat sollicitudin, tempor libero et, aliquet lectus. Mauris pretium tincidunt nisi ut congue. Nam quis mauris nec diam pellentesque lacinia sed in dolor. Fusce malesuada augue ultrices lobortis tristique.</p>
           <p>Nam quis urna sit amet magna hendrerit finibus. Proin id nibh felis. Nullam rutrum tortor in elit vestibulum, vel hendrerit sapien imperdiet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas volutpat accumsan ligula id condimentum. Pellentesque vel ligula sed ex porttitor eleifend. Nam pulvinar tristique felis sit amet vulputate.</p>
-          <p>Curabitur convallis ac massa non blandit. Fusce vitae eleifend nunc. Sed congue finibus odio, ut vestibulum orci luctus sit amet. Nunc at risus arcu. Proin feugiat lacinia ligula vitae porta. Curabitur vitae eros in lorem sagittis finibus. Nulla eleifend ex fringilla, gravida nisl in, gravida quam.</p>
         </div>
           {data}
       </div>
